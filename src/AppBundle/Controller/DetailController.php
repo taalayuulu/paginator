da88309cdb717b413a48ed3a8fcd72686774ecf6
kanat <taalayuulu@inbox.ru>
2017-06-02 15:11:22 +0600

    add new route

--- src/AppBundle/Controller/DetailController.php
+++ src/AppBundle/Controller/DetailController.php
@@ -8,11 +8,17 @@
 
 namespace src\AppBundle\Controller;
 
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DetailController extends Controller
 {
+
+    /**
+     * @Route("/view" ,name="view")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+
     public function indexAction() {
         $image = 'mers.JPG';
         return $this->render('detail/index.html.twig' , [
